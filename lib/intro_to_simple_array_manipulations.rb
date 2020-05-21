e7960c1dcc106c3566821b4c09f65aae2f7219f6
Michael Jester <m.jester.93@gmail.com>
2020-05-21 21:18:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,5 +15,9 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  array.delete(string, 2)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
